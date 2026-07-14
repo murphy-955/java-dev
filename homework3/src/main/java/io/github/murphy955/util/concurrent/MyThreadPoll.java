e878b96a990621b15f27e34f026be7c5946ebaa3
zeyuli <13776528859@163.com>
2026-07-14 16:39:22 +0800

    feat(threadpool): 实现自定义线程池核心功能
    - 添加 Worker 类用于封装工作线程
    - 添加execute方法注释

--- homework3/src/main/java/io/github/murphy955/util/concurrent/MyThreadPoll.java
+++ homework3/src/main/java/io/github/murphy955/util/concurrent/MyThreadPoll.java
@@ -3,7 +3,9 @@ package io.github.murphy955.util.concurrent;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Set;
 import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.LinkedBlockingDeque;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -25,6 +27,8 @@ public class MyThreadPoll {
     private TimeUnit timeUnit;
     // 拒绝策略
     private DenyPolicy denyPolicy;
+    // 工作线程
+    private Set<Worker> workerSet;
 
     public MyThreadPoll(BlockingDeque<Runnable> queue, int corePoolSize, int maximumPoolSize,
                         long keepAliveTime, TimeUnit timeUnit, DenyPolicy denyPolicy) {
@@ -34,6 +38,15 @@ public class MyThreadPoll {
         this.keepAliveTime = keepAliveTime;
         this.timeUnit = timeUnit;
         this.denyPolicy = denyPolicy;
+    }
+
+    public MyThreadPoll(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit timeUnit, DenyPolicy denyPolicy) {
+        this.corePoolSize = corePoolSize;
+        this.maximumPoolSize = maximumPoolSize;
+        this.keepAliveTime = keepAliveTime;
+        this.timeUnit = timeUnit;
+        this.denyPolicy = denyPolicy;
+        this.queue = new LinkedBlockingDeque<>(1000);
     }
 
     /**
@@ -58,5 +71,53 @@ public class MyThreadPoll {
         DISCARD_OLDEST("DiscardOldestPolicy: 丢弃队列最老任务，然后重试提交");
 
         private final String description;
+    }
+
+    /**
+     * <ol>
+     *     <li>workerSet.size() < corePoolSize&&queue.offer(task) == true，创建新线程执行任务</li>
+     *     <li>workerSet.size() >= corePoolSize&&queue.offer(task) == true，将任务加入队列</li>
+     *     <li>workerSet.size() >= maximumPoolSize&&queue.offer(task)==false，执行拒绝策略</li>
+     * </ol>
+    * @author 李泽聿
+    * @since 2026-07-14 16:30
+    */
+    public void execute(Runnable task) {
+    }
+}
+
+final class Worker implements Runnable{
+    private Runnable firstTask;
+
+    Worker(Runnable firstTask) {
+        this.firstTask = firstTask;
+    }
+
+    void start() {
+        Thread thread = new Thread(this);
+        thread.start();
+    }
+
+    @Override
+    public void run() {
+        while (firstTask != null || (firstTask = getTask()) != null) {
+            try {
+                firstTask.run();
+            } finally {
+                firstTask = null;
+            }
+        }
+        // 退出时从 workers 中移除
+        workers.remove(this);
+    }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
     }
 }
