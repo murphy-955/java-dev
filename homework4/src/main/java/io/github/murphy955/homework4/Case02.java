5c231f5be289b02a465c6ae6186ac07808bcb19f
zeyuli <13776528859@163.com>
2026-08-14 15:56:45 +0800

    feat: 补充事务方式，完成case02

--- homework4/src/main/java/io/github/murphy955/homework4/Case02.java
+++ homework4/src/main/java/io/github/murphy955/homework4/Case02.java
@@ -4,6 +4,9 @@ import io.github.murphy955.homework4.config.RedisConfig;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.Transaction;
+
+import java.util.List;
 
 /**
  * 同时更新订单状态和当天已支付统计计数
@@ -33,6 +36,30 @@ public class Case02 {
             String orderCountKey = BASE_ORDER_COUNT_KEY + id;
             Object result = redis.eval(LUA_SCRIPT, 2, orderStatusKey, orderCountKey, OrderStatus.PAID.getStatus());
             return (Long) result;
+        }
+    }
+
+    public static Long updateOrderStatusAndCountUseTX(Long id) {
+        String orderStatusKey = BASE_ORDER_STATUS_KEY + id;
+        String orderCountKey = BASE_ORDER_COUNT_KEY + id;
+        try (Jedis redis = RedisConfig.getJedis()) {
+            redis.watch(orderStatusKey);
+            String currentStatus = redis.get(orderStatusKey);
+            if (currentStatus == null) {
+                return -1L;
+            }
+            if (currentStatus.equals(OrderStatus.PAID.getStatus())) {
+                return -1L;
+            }
+            Transaction tx = redis.multi();
+
+            tx.set(orderStatusKey, OrderStatus.PAID.getStatus());
+            tx.incr(orderCountKey);
+            List<Object> exec = tx.exec();
+            if (exec == null) {
+                return -1L;
+            }
+            return (Long) exec.getFirst();
         }
     }
 
